27da421368800a41d2378ed881b6177f2c4e6e2f
Rafał Szymura <raszymura@gmail.com>
2024-05-26 10:41:59 +0200

    inter_less1_exer1 DONE

--- lesson1/inter_less1_exer1/src/main.c
+++ lesson1/inter_less1_exer1/src/main.c
@@ -25,12 +25,30 @@ static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
 /* STEP 2.3 - Create the expiry function for the timer */
+static void timer0_handler(struct k_timer *dummy)
+{
+    /*Interrupt Context - System Timer ISR */
+    static bool flip= true;
+    if (flip)
+        gpio_pin_toggle_dt(&led0);
+    else
+        gpio_pin_toggle_dt(&led1);
+
+    flip = !flip;
+}
 
 /* STEP 2.1 - Define the timer */
+K_TIMER_DEFINE(timer0, timer0_handler, NULL);
 
 /* STEP 3.1 - Define the data type of the message */
+typedef struct {
+    uint32_t x_reading;
+    uint32_t y_reading;
+    uint32_t z_reading;
+} SensorReading;
 
 /* STEP 3.2 - Define the message queue */
+K_MSGQ_DEFINE(device_message_queue, sizeof(SensorReading), 16, 4);
 
 int main(void)
 {
@@ -49,6 +67,8 @@ int main(void)
 		return 0;
 	}
 	/* STEP 2.2 - Start the timer  */
+	/* start periodic timer that expires once every 0.5 second  */
+	k_timer_start(&timer0, K_MSEC(500), K_MSEC(500));
 
 	return 0;
 }
@@ -63,6 +83,10 @@ static void producer_func(void *unused1, void *unused2, void *unused3)
 		static SensorReading acc_val = {100, 100, 100};
 		int ret;
 		/* STEP 3.3 - Write messages to the message queue */
+		ret = k_msgq_put(&device_message_queue,&acc_val,K_FOREVER);
+        if (ret){
+            LOG_ERR("Return value from k_msgq_put = %d",ret);
+        }
 
 		acc_val.x_reading += 1;
 		acc_val.y_reading += 1;
@@ -81,6 +105,10 @@ static void consumer_func(void *unused1, void *unused2, void *unused3)
 		SensorReading temp;
 		int ret;
 		/* STEP 3.4 - Read messages from the message queue */
+		ret = k_msgq_get(&device_message_queue,&temp,K_FOREVER);
+        if (ret){
+            LOG_ERR("Return value from k_msgq_get = %d",ret);
+        }
 
 		LOG_INF("Values got from the queue: %d.%d.%d\r\n", temp.x_reading, temp.y_reading,
 			temp.z_reading);
